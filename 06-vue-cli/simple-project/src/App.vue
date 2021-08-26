888e1f33d799668379f25a1f6795acba9a280d01
itallosavieira <itallo.sa.vieira@gmail.com>
2021-08-26 11:11:14 -0300

    update repo

---
+++ 06-vue-cli/simple-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<h1>{{ title }}</h1>
+		<button @click="title += ' # ' "> ala </button>
+	</div>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			title: 'Hello VueJS!'
+		}
+	}
+};
+</script>
