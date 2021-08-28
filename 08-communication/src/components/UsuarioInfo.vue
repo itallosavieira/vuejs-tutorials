7d05fb854d4a565510405bc1a0e3825107cf8b66
itallosavieira <itallo.sa.vieira@gmail.com>
2021-08-28 12:11:35 -0300

    add communication props, $emit/$event

---
+++ 08-communication/src/components/UsuarioInfo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuario: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            //default: function() {
+            //    return Array(5).fill(0).join('-')
+            //}
+            // required: true
+            default: 'Anonimo'
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Itallo'
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
