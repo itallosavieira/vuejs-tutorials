4b415af9ef82357b0b61ddfec39094ac711e6750
itallosavieira <itallo.sa.vieira@gmail.com>
2021-08-29 13:50:02 -0300

    update repo

--- 00-Projects/todo-list/src/bus.js
+++ 00-Projects/todo-list/src/bus.js
@@ -3,7 +3,6 @@ import Vue from 'vue'
 export default new Vue({
 	methods: {
 		setInputedTask(task) {
-			console.log(task)
 			this.$emit('inputedTask', task)
 		},
 		onInputedTask(callback) {
--- 00-Projects/todo-list/src/components/Tasks.vue
+++ 00-Projects/todo-list/src/components/Tasks.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="tasks">
     <ul>
-      <li @click="doneTask(i)"
-			class="task"
-			v-for="(task, i) in tasks" :key="i">
+      <li
+        @click="doneTask(i)"
+        class="task"
+        :class="[task.done ? 'done' : 'default']"
+        v-for="(task, i) in tasks"
+        :key="i"
+      >
         <span> {{ task.title }} </span>
-        <span @click="deleteTask(task)" class="remove-btn"> x </span>
+        <div @click="deleteTask(task, i)" class="remove-btn">
+          <span>+</span>
+        </div>
       </li>
     </ul>
   </div>
@@ -26,8 +32,10 @@ export default {
     });
   },
   methods: {
-    deleteTask(task) {
-      this.tasks.splice(this.tasks.indexOf(task), 1);
+    deleteTask(task, i) {
+      setTimeout(() => {
+        this.tasks.splice(this.tasks.indexOf(task), 1);
+      }, 100);
     },
     doneTask(i) {
       console.log(this.tasks[i].done, this.tasks[i].title);
@@ -55,13 +63,11 @@ export default {
   gap: 1rem;
   flex-wrap: wrap;
 }
-
 .task {
   list-style: none;
   border-radius: 10px;
   height: 100px;
   width: 250px;
-  background-color: #fff;
   box-shadow: rgb(0 0 0 / 10%) 0px 10px 15px -3px,
     rgb(0 0 0 / 5%) 0px 4px 6px -2px;
   padding: 0 2rem 0 1rem;
@@ -72,30 +78,49 @@ export default {
   position: relative;
   font-size: 1.15rem;
   cursor: pointer;
+  transition: all .25s;
 }
 
+.task:hover {
+  box-shadow: rgb(0 0 0 / 20%) 0px 10px 15px -3px,
+    rgb(0 0 0 / 5%) 0px 4px 6px -2px;
+}
+
+.task:active {
+  box-shadow: rgb(0 0 0 / 5%) 0px 10px 15px -3px,
+    rgb(0 0 0 / 5%) 0px 4px 6px -2px;
+}
+
+.default {
+  background-color: #fff;
+}
 .done {
   text-decoration: line-through;
   background-color: rgb(141, 228, 141);
 }
 
-.task .remove-btn {
+.remove-btn {
   right: 0;
   height: 100%;
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
+  border-left: 1px solid transparent;
   width: 10%;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #ff5a5a;
   opacity: 0.8;
-  color: #fff;
   position: absolute;
   transition: all 0.25s;
 }
 
+.remove-btn span {
+  font-weight: 600;
+  transform: rotate(45deg);
+}
+
 .task .remove-btn:hover {
-  opacity: 1;
+  background: #ff5a5a;
+  color: #fff;
 }
 </style>
