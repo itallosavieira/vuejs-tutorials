00f52eda803647d2b61fb6881a1a31ca91aa987e
itallosavieira <itallo.sa.vieira@gmail.com>
2021-08-31 11:01:55 -0300

    add 10-forms

--- 08-exercise/communication-challenge/src/components/UsuarioDetalhe.vue
+++ 08-exercise/communication-challenge/src/components/UsuarioDetalhe.vue
@@ -1,34 +1,34 @@
 <template>
-    <div class="usuario-detalhe">
-        <h2 v-if="!user">Usuário não selecionado!</h2>
-        <div v-else>
-            <h2>ID: {{ user.id }}</h2>
-            <h2>Nome: {{ user.nome }}</h2>
-            <h2>Idade: {{ user.idade }}</h2>
-        </div>
+  <div class="usuario-detalhe">
+    <h2 v-if="!user">Usuário não selecionado!</h2>
+    <div v-else>
+      <h2>ID: {{ user.id }}</h2>
+      <h2>Nome: {{ user.nome }}</h2>
+      <h2>Idade: {{ user.idade }}</h2>
     </div>
+  </div>
 </template>
 
 <script>
-import bus from '@/bus'
+import bus from "@/bus";
 
 export default {
-    data() {
-        return {
-            user: null
-        }
-    },
-    created() {
-        bus.onSelectedUser(user => {
-            this.user = user
-        })
-    }
-}
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+	bus.onInputedTask(task => {
+      this.tasks.push(task)
+    });
+  },
+};
 </script>
 
 <style>
-    .usuario-detalhe {
-        flex: 1;
-        border: 1px solid #CCC;
-    }
+.usuario-detalhe {
+  flex: 1;
+  border: 1px solid #ccc;
+}
 </style>
