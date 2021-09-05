0e8e8cb88cb95b4cc4be4c411879c4e4c5f98418
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-05 11:14:28 -0300

    Add local directives

--- 11-directives/exercises/src/App.vue
+++ 11-directives/exercises/src/App.vue
@@ -2,8 +2,12 @@
 	<div id="app">
 		<h1>Diretivas</h1>
 		<hr>
-		<p v-destaque:bg="color" v-text="'teste teste teste'"></p>
-		<p v-destaque.alternate.delay="color" v-text="'teste teste teste'"></p>
+		<p
+			v-destaque-local:bg="{color1: color}"
+			v-text="'teste teste teste'"></p>
+		<p
+			v-destaque-local.delay.alternate="{color1: 'lightgreen', color2: 'purple', delay: 5000, interval: 200}"
+			v-text="'teste teste teste'"></p>
 	</div>
 </template>
 
@@ -12,6 +16,37 @@ export default {
 	data() {
 		return {
 			color: 'lightgreen'
+		}
+	},
+	directives: {
+		'destaque-local': {
+			bind(el, binding) {
+				let delay = 0
+				if(binding.modifiers['delay']) delay = binding.value.delay
+
+				const color1 = binding.value.color1
+				const color2 = binding.value.color2
+				let currentColor = color1
+
+				function applyColor(color) {
+					if(binding.arg === 'bg') {
+						el.style.backgroundColor = color
+					} else {
+						el.style.color = color
+					}
+				}
+
+				setTimeout(() => {
+					if(binding.modifiers['alternate']) {
+						setInterval(() => {
+							currentColor = currentColor === color1 ? color2 : color1
+							applyColor(currentColor)
+						}, binding.value.interval)
+					} else {
+						applyColor(color1)
+					}
+				}, delay);
+			}
 		}
 	}
 }
--- 11-directives/exercises/src/main.js
+++ 11-directives/exercises/src/main.js
@@ -1,39 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('destaque', {
-	bind(el, binding, vnode) {
-		let delay = 0
-		let interval = 1000
-
-		const color1 = binding.value
-		const color2 = 'purple'
-		let currentColor = color1
-
-		function applyColor(color) {
-			if(binding.arg === 'bg') {
-				el.style.backgroundColor = color
-			} else {
-				el.style.color = color
-			}
-		}
-
-		if(binding.modifiers['delay']) delay = 3000
-
-		if(binding.modifiers['alternate']) {
-			setTimeout(() => {
-				setInterval(() => {
-					currentColor = currentColor === color1 ? color2 : color1
-					applyColor(color)
-				}, interval)
-				applyColor(color)
-			}, delay);
-		}
-
-
-	}
-})
-
 Vue.config.productionTip = false
 
 new Vue({
