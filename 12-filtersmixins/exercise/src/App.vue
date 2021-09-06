576a2a017c2285290c0c06b44db082f3c0137136
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-06 08:14:12 -0300

    Add cpf local filter

--- 12-filtersmixins/exercise/src/App.vue
+++ 12-filtersmixins/exercise/src/App.vue
@@ -1,12 +1,27 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<h2>{{ myCpf | cpf }}</h2>
 	</div>
 </template>
 
 <script>
 export default {
-
+	filters: {
+		cpf(value) {
+			const arr = value.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
+	data() {
+		return {
+			myCpf: '44333309867'
+		}
+	}
 }
 </script>
 
