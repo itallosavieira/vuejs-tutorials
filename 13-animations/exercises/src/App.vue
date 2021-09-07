ca9da240ba39034ff4d75e16816917c5e39a9793
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-07 08:17:01 -0300

    Add 13-transitions exercise

---
+++ 13-animations/exercises/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animations</h1>
+		<hr>
+		<b-button
+			variant="primary" class="mb-4"
+			@click="show = !show">Show Message</b-button>
+
+		<transition name="fade">
+			<b-alert
+				variant="info" show
+				v-if="show">{{ message }}</b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			message: 'An info message for the user',
+			show: false
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+
+.fade-enter {
+
+}
+.fade-enter-active {
+
+}
+.fade-enter-to {
+
+}
+.fade-leave {
+
+}
+.fade-leave-active {
+
+}
+.fade-leave-to {
+
+}
+</style>
