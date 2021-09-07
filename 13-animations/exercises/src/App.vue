1afaeee61317cc0d775db0b26c5470f3c42c1575
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-07 18:07:27 -0300

    Update repo

--- 13-animations/exercises/src/App.vue
+++ 13-animations/exercises/src/App.vue
@@ -6,16 +6,25 @@
 			variant="primary" class="mb-4"
 			@click="show = !show">Show Message</b-button>
 
+		<!--
 		<transition name="fade">
 			<b-alert
 				variant="info" show
 				v-if="show">{{ message }}</b-alert>
 		</transition>
+		-->
+		<transition name="slide" mode="out-in">
+			<b-alert variant="info" show v-if="show" key="info">{{ message }}</b-alert>
+			<b-alert variant="warning" show v-else key="warning">{{ message }}</b-alert>
+		</transition>
 
-		<transition name="slide">
-			<b-alert
-				variant="info" show
-				v-if="show">{{ message }}</b-alert>
+		<hr>
+
+
+		<button @click="show2 = !show2">Action</button>
+		<transition>
+
+			<div v-if="show2" class="box"></div>
 		</transition>
 	</div>
 </template>
@@ -26,7 +35,27 @@ export default {
 	data() {
 		return {
 			message: 'An info message for the user',
-			show: false
+			show: false,
+			show2: false
+		}
+	},
+	methods: {
+		beforeEnter(el) {
+		},
+		enter(el, done) {
+
+		},
+		afterEnter(el) {
+		},
+		enterCancelled(el) {
+		},
+		beforeLeave(el) {
+		},
+		leave(el, done) {
+		},
+		afterLeave(el) {
+		},
+		leaveCancelled(el) {
 		}
 	}
 }
@@ -73,6 +102,17 @@ export default {
 
 .slide-enter, .slide-leave-to {
 	opacity: 0;
+}
+
+hr {
+	margin: 50px 0;
+}
+
+.box {
+	margin: 20px auto;
+	height: 100px;
+	width: 300px;
+	background-color: lightgreen;
 }
 
 </style>
