1cba9b8d1e1c34997b54d8d449b772ec30b95c3d
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-12 17:55:35 -0300

    Config my first route with vue-router, view and link

---
+++ vue-routing/src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
+import App from './App.vue';
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/users',
+			component: UsersList
+		},
+		{
+			path: '/teams',
+			component: TeamsList
+		}
+	]
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app');
