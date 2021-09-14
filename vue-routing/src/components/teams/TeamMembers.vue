baa69e1521bf6874bba3c6d55b2e8f15e75eab8c
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-14 16:44:42 -0300

    Add logic of dynamic routes

--- vue-routing/src/components/teams/TeamMembers.vue
+++ vue-routing/src/components/teams/TeamMembers.vue
@@ -1,52 +1,62 @@
 <template>
-  <section>
-    <h2>{{ teamName }}</h2>
-    <ul>
-      <user-item
-        v-for="member in members"
-        :key="member.id"
-        :name="member.fullName"
-        :role="member.role"
-      ></user-item>
-    </ul>
-  </section>
+	<section>
+		<h2>{{ teamName }}</h2>
+		<ul>
+			<user-item
+				v-for="member in members"
+				:key="member.id"
+				:name="member.fullName"
+				:role="member.role"
+			></user-item>
+		</ul>
+	</section>
 </template>
 
 <script>
 import UserItem from '../users/UserItem.vue';
 
 export default {
-  components: {
-    UserItem
-  },
-  data() {
-    return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
-    };
-  },
+	inject: ['users', 'teams'],
+	components: {
+		UserItem,
+	},
+	data() {
+		return {
+			teamName: '',
+			members: [],
+		};
+	},
+	created() {
+		const teamId = this.$route.params.teamId; //t1
+		const selectedTeam = this.teams.find((team) => team.id === teamId); //t1
+		const members = selectedTeam.members; // t1.members = u1, u2
+		const selectedMembers = [];
+		for (let member of members) {
+			const selectedUser = this.users.find((user) => user.id == member);
+			selectedMembers.push(selectedUser);
+		}
+		this.members = selectedMembers;
+		this.teamName = selectedTeam.name;
+	},
 };
 </script>
 
 <style scoped>
 section {
-  margin: 2rem auto;
-  max-width: 40rem;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-  border-radius: 12px;
+	margin: 2rem auto;
+	max-width: 40rem;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+	padding: 1rem;
+	border-radius: 12px;
 }
 
 h2 {
-  margin: 0.5rem 0;
+	margin: 0.5rem 0;
 }
 
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+	list-style: none;
+	margin: 0;
+	padding: 0;
 }
 </style>
--- vue-routing/src/main.js
+++ vue-routing/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import TeamsList from './components/teams/TeamsList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import UsersList from './components/users/UsersList.vue';
 
 import App from './App.vue';
@@ -9,12 +10,16 @@ const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{
-			path: '/users',
+			path: '/users/',
 			component: UsersList
 		},
 		{
 			path: '/teams',
 			component: TeamsList
+		},
+		{
+			path: '/teams/:teamId',
+			component: TeamMembers
 		}
 	]
 });
