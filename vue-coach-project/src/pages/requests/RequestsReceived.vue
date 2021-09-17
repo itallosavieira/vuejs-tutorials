227438c5ed8edf5bc79bfe1c8621e34c7f653944
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-17 19:47:50 -0300

    Add request-otem component

--- vue-coach-project/src/pages/requests/RequestsReceived.vue
+++ vue-coach-project/src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,51 @@
-<template>REQUESTS</template>
+<template>
+	<section>
+		<base-card>
+			<header>
+				<h2>Requests Received</h2>
+			</header>
+			<ul>
+				<request-item
+					v-for="req in receivedRequests"
+					:key="req.id"
+					:message="req.userMessage"
+					:email="req.userEmail"
+				>
+				</request-item>
+			</ul>
+			<h3>You have not received any requests yet.</h3>
+		</base-card>
+	</section>
+</template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+export default {
+	components: { RequestItem },
+	computed: {
+		receivedRequests() {
+			return this.$store.getters['req/requests'];
+		},
+		hasRequests() {
+			return this.$store.getters['req/hasRequests'];
+		},
+	},
+};
+</script>
+
+<style scoped>
+header {
+	text-align: center;
+}
+
+ul {
+	list-style: none;
+	margin: 2rem auto;
+	padding: 0;
+	max-width: 30rem;
+}
+
+h3 {
+	text-align: center;
+}
+</style>
