c2f3f670321ec0f2b9b57fa7f1c3eb49171270a3
itallosavieira <itallo.sa.vieira@gmail.com>
2021-09-17 17:00:14 -0300

    Add Register and Form

--- vue-coach-project/src/pages/coaches/CoachesList.vue
+++ vue-coach-project/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,7 @@
 <template>
-	<section>FILTER</section>
+	<section>
+		<coach-filter @change-filter="setFilters"></coach-filter>
+	</section>
 	<base-card>
 		<div class="controls">
 			<base-button mode="outline">Refresh</base-button>
@@ -23,19 +25,45 @@
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
-import BaseButton from '../../components/UI/BaseButton.vue';
+import CoachFilter from '../../components/coaches/CoachFilter.vue';
 
 export default {
 	components: {
 		CoachItem,
-		BaseButton,
+		CoachFilter,
+	},
+	data() {
+		return {
+			activeFilters: {
+				frontend: true,
+				backend: true,
+				career: true,
+			},
+		};
 	},
 	computed: {
 		filteredCoaches() {
-			return this.$store.getters['coaches/coaches'];
+			const coaches = this.$store.getters['coaches/coaches'];
+			return coaches.filter((coach) => {
+				if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+					return true;
+				}
+				if (this.activeFilters.backend && coach.areas.includes('backend')) {
+					return true;
+				}
+				if (this.activeFilters.career && coach.areas.includes('career')) {
+					return true;
+				}
+				return false;
+			});
 		},
 		hasCoaches() {
 			return this.$store.getters['coaches/hasCoaches'];
+		},
+	},
+	methods: {
+		setFilters(updatedFilters) {
+			this.activeFilters = updatedFilters;
 		},
 	},
 };
